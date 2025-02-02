44ddf6e0537dd68cedb3f44239379b46f27ed1ab
jimmy <jimlovebao@gmail.com>
2025-02-02 04:22:52 -0500

    working on update slice

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,36 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    
+    cart.forEach((item) => {
+        totalCost += parseInt(item.cost.slice(1)) * item.quantity;
+    });
+    console.log(totalCost);
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item.quantity++));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity(item.quantity--));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseInt(item.cost.slice(1)) * item.quantity;
   };
 
   return (
